2c93f11e95908015bd89228560821cf58a90c535
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-10-06 15:21:14 +0300

    feat: add code in delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import {rm} from 'fs/promises'
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+
+const filename = fileURLToPath(import.meta.url);
+const dirName = dirname(filename);
+
 const remove = async () => {
-    // Write your code here 
+    const fileToDeletePath = join(dirName, 'files','fileToRemove.txt');
+    try{
+        await rm(fileToDeletePath)
+    }
+    catch(err) {
+        console.log(err)
+        throw new Error("FS operation failed")
+    }
 };
 
 await remove();
