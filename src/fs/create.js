ac3edefa9351f3953ab389eaf4b4d729de4a9d35
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-10-06 08:25:02 +0300

    feat: add code for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { appendFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { existsSync } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fresh.txt'); 
+    
+try {
+    if (existsSync(filePath)){
+        console.log('The path exists.');//
+        throw new Error('FS operation failed');
+    }
+    await appendFile(filePath, 'I am fresh and young')
+}
+catch(err) {
+    throw err;
+}
 };
 
 await create();
