e24c48cd7a9e966a80f5465f7676964e36c36d45
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-10-07 14:20:17 +0300

    feat: add code for write-strem task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = new URL('./files/fileToWrite.txt', import.meta.url);
+    const output = createWriteStream(filePath);
+    stdin.pipe(output);
+    output.on('finish', () => {
+        process.exit();
+    });
 };
 
 await write();
