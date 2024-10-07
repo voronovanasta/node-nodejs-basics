e912d00d6137ad936c5503b5fff52199afb5ff5f
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-10-07 16:02:27 +0300

    feat: add code in zip folder

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { pipeline } from 'node:stream';
+import { createGunzip  } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const dest = new URL('./files/fileToCompress.txt', import.meta.url);
+    const source = new URL('./files/archive.gz', import.meta.url);
+    const input = createReadStream(source);
+    const output = createWriteStream(dest);
+
+    const decompressGzip = createGunzip();
+    pipeline(
+        input,
+        decompressGzip,
+        output,
+        (err) => {
+            if (err) {
+              console.error('Pipeline failed.', err);
+            } else {
+              console.log('Pipeline succeeded.');
+            }
+          }
+    )
 };
 
 await decompress();
