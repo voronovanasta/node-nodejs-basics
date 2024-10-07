3c0e82b7e793a42c45d634d3c70653418121d146
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-10-07 12:35:40 +0300

    feat: add code in hash folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
+
 const calculateHash = async () => {
-    
+  const hash = createHash('sha256');
+  const filePath = new URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+  const fileData = createReadStream(filePath);
+    fileData.on('readable', () => {
+  const data = fileData.read();
+  if (data)
+  hash.write(data);
+  hash.end();
+});
+
+hash.on('readable', () => {
+    const data = hash.read();
+    if (data) {
+      console.log(data.toString('hex'));
+    }
+  });
 };
 
 await calculateHash();
+
+
